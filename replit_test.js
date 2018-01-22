5d749e73dadf796adf1a8e1a0226009916c5e776
lia <35627990+satanlia@users.noreply.github.com>
2018-01-22 05:02:25 +0200

    Delete replit_test.js

--- replit_test.js
+++
@@ -1,7 +0,0 @@
-var repl = new ReplitClient('api.repl.it', 80, 'ruby', token);
-repl.connect().then(() =>
-  repl.evaluate(
-    'puts "hello worls"',
-     { stdout: out => console.log(out) }
-  );
-});
