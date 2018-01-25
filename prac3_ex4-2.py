52ecc2926a22d90e081f8ebdaad37853bf594543
lia <35627990+satanlia@users.noreply.github.com>
2018-01-25 15:56:23 +0200

    Create prac3_ex4-2.py

---
+++ prac3_ex4-2.py
@@ -0,0 +1,8 @@
+#Изменить код так, чтобы в конце каждой буквы добавлялась точка.
+
+s = ['yesterday', 'today', 'tomorrow']
+
+for  i  in  s:   
+   print()
+   for  j  in  i:
+      print(j, end=".")
